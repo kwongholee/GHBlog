93dfabbac2e2810b16a2ae6b15e44866a3b1811f
kwongholee <ghlee9883@gmail.com>
2023-08-02 13:45:43 +0900

    8.2 스타일 조정

--- app/List.js
+++ app/List.js
@@ -16,32 +16,28 @@ export default function List(props) {
             <div className="bestList">
                 <div className={style.centeredContainer}>
                     <FontAwesomeIcon icon={faTrophy} style={{color: "#f7e418",}} size="3x" />
-                    <h1 className={style.bestListTitle}>Best 3</h1>
+                    <h1 className={style.bestListTitle}>Best 5</h1>
                     <FontAwesomeIcon icon={faTrophy} style={{color: "#f7e418",}} size="3x" />
                 </div>
                 {
                     list.map((a,i) => {
                         let colorList = ["#ede735", "#a6a6a6", "#63440d", "#af48f4", "#000000"];
                         return(
-                            <div className="list-item" key={i} onClick={() => {router.push('/detail/' + a._id);}}>
-                                <div style={{display: 'flex', justifyContent: 'space-between'}}>
-                                    <h4 style={{marginRight: 'auto'}}>{a.title}</h4>
-                                    <p style={{marginLeft: 'auto'}}>{a.date}</p>
+                            <div key={i}>
+                                <div className="list-item" onClick={() => {router.push('/detail/' + a._id);}}>
+                                    <FontAwesomeIcon icon={faMedal} beat style={{color: colorList[i],}} size="3x" />
+                                    <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                                        <h4 style={{marginRight: 'auto'}}>{a.title}</h4>
+                                        <p style={{marginLeft: 'auto'}}>{a.date}</p>
+                                    </div>
+                                    <p>{a.writer}</p>
+                                    <p style={{position: 'absolute', left: '1.5%'}}>👍: {a.like}</p>
                                 </div>
-                                <p>{a.writer}</p>
-                                <p style={{position: 'absolute', left: '1.5%'}}>👍: {a.like}</p>
                             </div>
                         )
                     })
                 }
-                <FontAwesomeIcon icon={faMedal} beat style={{color: "#a6a6a6",}} />
             </div>
         </div>
     )
 }
-
-// 1등  <FontAwesomeIcon icon={faMedal} beat style={{color: "#ede735",}} />
-// 2등  <FontAwesomeIcon icon={faMedal} beat style={{color: "#a6a6a6",}} />
-// 3등  <FontAwesomeIcon icon={faMedal} beat style={{color: "#63440d",}} />
-// 4등  <FontAwesomeIcon icon={faMedal} beat style={{color: "#af48f4",}} />
-// 5등  <FontAwesomeIcon icon={faMedal} beat style={{color: "#000000",}} />
--- app/write/page.js
+++ app/write/page.js
@@ -1,11 +1,23 @@
+import style from './write.module.css'
+
 export default function Write() {
   return(
     <div>
-      <h1>Write Page</h1>
+      <h1 style={{textAlign: 'center'}}>Write Page</h1>
       <form action="/api/post/new" method="POST">
-        <input name="title" placeholder="title" style={{width: '20%'}} />
-        <textarea name="content" placeholder="content" cols={100} rows={20} style={{display: 'block'}} />
-        <button type="submit">작성 완료</button>
+        <div className={style.container}>
+          <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <label style={{flex: 1}}>title</label>
+            <input name="title" placeholder="title" className={style.titleInput} style={{flex: 'none'}} />
+          </div>
+          <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <label style={{flex: 1}}>content</label>
+            <textarea name="content" placeholder="content" cols={100} rows={20} className={style.contentInput} style={{flex: 'none'}} />
+          </div>
+          <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <button type="submit" className={style.inputBtn}>작성 완료</button>
+          </div>
+        </div>
       </form>
     </div>
   )
