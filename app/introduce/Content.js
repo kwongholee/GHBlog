447784652001f3a396fb1af8c577b91825db0ebe
kwongholee <ghlee9883@gmail.com>
2023-08-06 21:24:16 +0900

    8.6 useObserver 설정 중

--- app/introduce/Content.js
+++ app/introduce/Content.js
@@ -3,7 +3,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAnglesDown } from '@fortawesome/free-solid-svg-icons'
 import style from './introduce.module.css'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import Image from 'next/image'
 import introduce from '/public/introduceMe.jpeg'
@@ -11,24 +11,22 @@ import NEXTJS from '/public/nextJSframework.png'
 import smile from '/public/smile.jpeg'
 
 export default function Content() {
-  // let [count, setCount] = useState(0);
-
-  // useEffect(() => {
-  //   let observer = new IntersectionObserver((e) => {
-  //     e.forEach((a) => {
-  //       if(a.isIntersecting) {
-  //         a.target.style.animation = 'titleChange 1.5s';
-  //       } else {
-  //         a.target.style.animation = 'titleDisappear 1.5s';
-  //       }
-  //     })
-  //   });
-  //   observer.observe(document.getElementById('first'))
-  //   setCount(1);
-  //   return() => {
-  //     setCount(0);
-  //   }
-  // }, [count]);
+  const target = useRef(null)
+  useEffect(() => {
+    let observer;
+    if(target.current) {
+      observer = new IntersectionObserver(([e]) => {
+        const targetElement = e.target;
+        if(e.isIntersecting) {
+          targetElement.style.animation = 'titleChange 1.5s';
+        } else {
+          targetElement.style.animation = 'titleDisappear 1.5s';
+        }
+      }, {threshold: 0});
+      observer.observe(target.current)
+      console.log('IntersectionObserver is observing:', target.current);
+    }
+  }, [target]);
   
   return(
     <div>
@@ -39,12 +37,12 @@ export default function Content() {
 
       <div>
         <div className={style.comment1}>My Name is 이광호</div>
-        <Image src={introduce} width={1000} className={style.comment1Image} />
+        <Image src={introduce} width={1000} className={style.comment1Image} alt='핑구' />
       </div>
 
       <div>
         <div className={style.comment2}>I made this blog with NEXT.JS</div>
-        <Image src={NEXTJS} width={1000} className={style.comment2Image} />
+        <Image src={NEXTJS} width={1000} className={style.comment2Image} alt='nextjs' />
       </div>
 
       <div>
@@ -52,7 +50,7 @@ export default function Content() {
       </div>
 
       <div>
-        <Image src={smile} width={1000} className={style.comment4Image} />
+        <Image src={smile} width={1000} className={style.comment4Image} alt='뿌듯' />
         <div className={style.comment4}>이러면 좀 있어보이자나요</div>
       </div>
     </div>
